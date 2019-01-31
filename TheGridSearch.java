7f2bbf8f7544caa6fddc62e9a0a3aafe058ae6df
Héctor Del Campo <FoxeyYy@users.noreply.github.com>
2019-01-31 15:57:26 +0100

    Create TheGridSearch.java

---
+++ TheGridSearch.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    //O(rows*cols*rowsP*colsP) Time, O(1) Space
+    static String gridSearch(String[] G, String[] P) {
+
+        for(int i = 0; i <= G.length - P.length; i++) {
+            for (int j = 0;  j <= G[0].length() - P[0].length(); j++) {
+                for (int k = 0; k < P.length; k++) {
+                    if (!P[k].equals(G[k+i].substring(j, j + P[k].length()))) break;
+                    else if (k ==  P.length - 1) return "YES";
+                }
+            }
+        }
+
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] RC = scanner.nextLine().split(" ");
+
+            int R = Integer.parseInt(RC[0]);
+
+            int C = Integer.parseInt(RC[1]);
+
+            String[] G = new String[R];
+
+            for (int i = 0; i < R; i++) {
+                String GItem = scanner.nextLine();
+                G[i] = GItem;
+            }
+
+            String[] rc = scanner.nextLine().split(" ");
+
+            int r = Integer.parseInt(rc[0]);
+
+            int c = Integer.parseInt(rc[1]);
+
+            String[] P = new String[r];
+
+            for (int i = 0; i < r; i++) {
+                String PItem = scanner.nextLine();
+                P[i] = PItem;
+            }
+
+            String result = gridSearch(G, P);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
